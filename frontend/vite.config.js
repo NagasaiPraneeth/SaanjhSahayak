4f098af947234ef4a3d0d53135dd8ef206cfe41b
nagasai <nagasaipraneeth5@gmail.com>
2024-08-10 11:37:37 +0530

    Revert "."
    This reverts commit c965e0a39a451703bb3fa4bb9b3173eb79e4f3ed.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: '../backend/build',
-    chunkSizeWarningLimit: 1600,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-        }
-      }
-    }
+  build:{
+    outDir:'../backend/build'
   }
 })
