2b814f6b6fe66a3f05391ba9a9818e65c8408c52
nagasai <nagasaipraneeth5@gmail.com>
2024-08-19 13:35:52 +0530

    Update Main.jsx

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -8,16 +8,20 @@ import MedicalChatbot from './MedicalChatbot';
 
 export default function Main(props) {
     const [noofPatients, setNoofPatients] = useState(0);
-    const navigate = useNavigate();    
+    const [isDoctor, setIsDoctor] = useState();
+    const navigate = useNavigate();
+    useEffect(()=>{
+      setIsDoctor(props.isDoctor);
+    },[props.isDoctor]);     
       return (
         <div
           className="relative flex size-full min-h-screen flex-col bg-[#f8fafb] group/design-root overflow-x-hidden"
           style={{ fontFamily: 'Inter, "Noto Sans", sans-serif' }}>
           <div className="layout-container flex h-full grow flex-col">
-              <Navbar isDoctor={props.isDoctor} />
+              <Navbar isDoctor={isDoctor} />
             <div className="gap-1 px-6 flex flex-1 justify-center py-5">
                 
-                <List isDoctor={props.isDoctor} setNoofPatients={setNoofPatients} noofPatients={noofPatients}/>
+                <List isDoctor={isDoctor} setNoofPatients={setNoofPatients} noofPatients={noofPatients}/>
                 <Analytics noofPatients={noofPatients}/>
 
             </div>
